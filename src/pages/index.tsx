ae15014cc534bc6475b20000ee9846acfe5f8b54
RafaelManfrim <rafaelmanfrim2004@gmail.com>
2021-12-11 13:26:22 -0300

    Pagina home desenvolvida

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,25 @@
 import Head from 'next/head'
+import { SubscribeButton } from '../components/SubscribeButton'
+import styles from './home.module.scss'
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Início | Ignews</title>
+        <title>Home | Ignews</title>
       </Head>
-      <h1>Hello World</h1>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome! </span>
+          <h1>News about the <span>React</span> world.</h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for $4.99  month</span>
+          </p>
+          <SubscribeButton />
+        </section>
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
     </>
   )
 }
