bdb403d85793845b9f708359933421e85e4d9f2b
Rafael Manfrim <rafael.manfrim@etec.sp.gov.br>
2021-12-22 11:14:12 -0300

    exibindo posts do prismic

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -3,8 +3,20 @@ import Head from 'next/head'
 import { getPrismicClient } from '../../services/prismic'
 import styles from './styles.module.scss'
 import Prismic from '@prismicio/client'
+import { RichText } from 'prismic-dom'
 
-export default function Posts() {
+type Post = {
+    slug: string,
+    title: string,
+    excerpt: string
+    updatedAt: string
+}
+
+type PostsProps = {
+    posts: Post[]
+}
+
+export default function Posts({posts}: PostsProps) {
     return (
         <>
             <Head>
@@ -12,21 +24,13 @@ export default function Posts() {
             </Head>
             <main className={styles.container}>
                 <div className={styles.postList}>
-                    <a href="">
-                        <time>12 de março de 2021</time>
-                        <strong>Sobre nós</strong>
-                        <p>Criada a partir da demanda de agentes do setor de energia, a Nimbus é uma empresa de prestação de serviços que oferece informações essenciais para auxiliar seus clientes nas tomadas de decisão. O objetivo maior da Nimbus é a confiabilidade, agilidade, qualidade e transparência dos dados disponibilizados em sua plataforma.</p>
-                    </a>
-                    <a href="">
-                        <time>12 de março de 2021</time>
-                        <strong>Sobre nós</strong>
-                        <p>Criada a partir da demanda de agentes do setor de energia, a Nimbus é uma empresa de prestação de serviços que oferece informações essenciais para auxiliar seus clientes nas tomadas de decisão. O objetivo maior da Nimbus é a confiabilidade, agilidade, qualidade e transparência dos dados disponibilizados em sua plataforma.</p>
-                    </a>
-                    <a href="">
-                        <time>12 de março de 2021</time>
-                        <strong>Sobre nós</strong>
-                        <p>Criada a partir da demanda de agentes do setor de energia, a Nimbus é uma empresa de prestação de serviços que oferece informações essenciais para auxiliar seus clientes nas tomadas de decisão. O objetivo maior da Nimbus é a confiabilidade, agilidade, qualidade e transparência dos dados disponibilizados em sua plataforma.</p>
-                    </a>
+                    {posts.map(post => (
+                        <a href="" key={post.slug}>
+                            <time>{post.updatedAt}</time>
+                            <strong>{post.title}</strong>
+                            <p>{post.excerpt}</p>
+                        </a>
+                    ))}
                 </div>
             </main>
         </>
@@ -41,5 +45,18 @@ export const getStaticProps: GetStaticProps = async () => {
         fetch: ['post.title', 'post.content'],
         pageSize: 15
     })
-    return { props: response }
+
+    const posts = response.results.map(post => {
+        return {
+            slug: post.uid,
+            title: RichText.asText(post.data.title),
+            excerpt: post.data.content.find(content => content.type === 'paragraph')?.text ?? '',
+            updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-BR', {
+                day: '2-digit',
+                month: 'long',
+                year: 'numeric'
+            })
+        }
+    })
+    return { props: { posts}  }
 }
