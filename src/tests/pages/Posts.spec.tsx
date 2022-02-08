1aa6f9ce405b1089dd7aa7b20436ec73d085d0df
RafaelManfrim <rafael.manfrim@etec.sp.gov.br>
2022-02-08 16:19:49 -0300

    Testando pagina post

---
+++ src/tests/pages/Posts.spec.tsx
@@ -0,0 +1,42 @@
+import { render, screen } from '@testing-library/react'
+import { mocked } from 'jest-mock'
+import Posts, { getStaticProps } from '../../pages/posts'
+import { stripe } from '../../services/stripe'
+import { getPrismicClient } from '../../services/prismic'
+
+jest.mock("../../services/stripe")
+jest.mock("../../services/prismic")
+
+const posts = [
+    { slug: 'my-new-post', title: 'New post', excerpt: 'Post excerpt', updatedAt: '10 de Abril' }
+]
+
+describe('Posts page', () => {
+    test("renders correctly", () => {
+        render(<Posts posts={posts} />)
+
+        expect(screen.getByText("New post")).toBeInTheDocument()
+    })
+
+    test("loads initial data", async () => {
+        const getPrismicClientMocked = mocked(getPrismicClient)
+
+        getPrismicClientMocked.mockReturnValueOnce({
+            query: jest.fn().mockResolvedValueOnce({
+                results: [
+                    { uid: 'my-new-post', data: { title: [{ type: 'heading', text: 'My new post' }], content: [{ type: 'paragraph', text: 'Post excerpt' }] }, last_publication_date: '04-01-2021' }
+                ]
+            })
+        } as any)
+
+        const response = await getStaticProps({})
+
+        expect(response).toEqual(expect.objectContaining({
+            props: {
+                posts: [
+                    { slug: 'my-new-post', title: 'My new post', excerpt: 'Post excerpt', updatedAt: '01 de abril de 2021' }
+                ]
+            }
+        }))
+    })
+})
